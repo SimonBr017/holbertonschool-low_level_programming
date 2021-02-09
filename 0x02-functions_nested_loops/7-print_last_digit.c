74c37dfc9dbf201d5747d48341e12b420c92f7a2
SimonBr017 <simonbrard017@gmail.com>
2021-02-09 16:41:46 +0000

    fix 7 i guess

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -3,17 +3,15 @@
  *print_last_digit - print the last digit of a int r
  *@n: integer
  *
- *Return: returns r value of the las digit
+ *Return: returns r value of the last digit
  */
 int print_last_digit(int n)
 {
 	int x;
 
 	if (n < 0)
-	{
 		n = -n;
-		x = n % 10;
-	}
+	x = n % 10;
 	_putchar(x + '0');
 	return (x);
 }
