d24f0b1ca2536a678679ff7aa54c7c341c974c04
SimonBr017 <simonbrard017@gmail.com>
2021-02-09 12:44:24 +0000

    new code t7

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -2,11 +2,21 @@
 /**
  *print_last_digit - print the last digit of a int r
  *@r: integer
- *
+ *@a: the last digit
  *Return: returns r value of the las digit
  */
 int print_last_digit(int r);
 {
-	r = r % 10;
-	return (r);
+	int a;
+
+	if (r >= 0)
+	{
+		a = r % 10;
+	}
+	else
+	{
+		a = -r % 10;
+	}
+	_putchar(a + '0');
+	return (a);
 }
