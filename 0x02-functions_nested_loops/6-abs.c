aed2974ef6ec9002f1304891287625f9f2eec8a5
SimonBr017 <simonbrard017@gmail.com>
2021-02-09 12:32:07 +0000

    fix t6

--- 0x02-functions_nested_loops/6-abs.c
+++ 0x02-functions_nested_loops/6-abs.c
@@ -3,21 +3,14 @@
  *_abs - returns absolute value of integer
  *@r: r has in integer
  *
- *Return: 0
+ *Return: --r for r!=0
  */
-int _abs(int r);
+int _abs(int r)
 {
 	if (r > 0)
 	{
-		_putchar(r);
+		return (r);
 	}
-	else if (r == 0)
-	{
-		_putchar('0');
-	}
-	else  /*if r<0*/
-	{
-		_putchar(r * -1);
-	}
-	return (0);
+	else
+		return(-r);
 }
