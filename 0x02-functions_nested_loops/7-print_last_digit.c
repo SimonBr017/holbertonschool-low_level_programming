44607dc49adc1e008347b3ffbd15b0c4d457a880
SimonBr017 <simonbrard017@gmail.com>
2021-02-09 11:00:29 +0000

    add t7 and holberton uppdate

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,13 @@
+#include "holberton.h"
+/**
+ *print_last_digit - print the last digit of a int r
+ *@r: integer
+ *
+ *Return: returns r value of the las digit
+ */
+
+int print_last_digit(int r)
+{
+	u = u % 10;
+	return (r);
+}
