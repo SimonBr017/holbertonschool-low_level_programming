47f7cd035eb3b93028c684d1a9952f46c58a3d5d
SimonBr017 <simonbrard017@gmail.com>
2021-02-10 10:14:23 +0000

    fix t7 finally?

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -1,6 +1,6 @@
 #include "holberton.h"
 /**
- *print_last_digit - print the last digit of a int r
+ *print_last_digit - print the last digit of a int n
  *@n: integer
  *
  *Return: returns r value of the last digit
@@ -10,9 +10,9 @@ int print_last_digit(int n)
 	int x;
 
 	x = n % 10;
-	if (n < 0)
+	if (x < 0)
 	{
-	n = -n;
+	x = -x;
 	}
 	_putchar(x + '0');
 	return (x);
