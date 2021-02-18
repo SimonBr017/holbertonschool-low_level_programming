be124847b0cdde6156972cf0c4808183d88e5c17
SimonBr017 <simonbrard017@gmail.com>
2021-02-18 14:19:09 +0000

    t6 proper

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -6,19 +6,23 @@
  */
 char *cap_string(char *str)
 {
-	int i;
-
+	int i,j;
+	char *c = " \t\n,;.!?\"(){}";
+ 
 	for (i = 0; str[i] != '\0'; i++)
 	{
 		if (str[0] >= 97 && str[0] <= 122)
 		{
 			str[0] = str[0] - 32;
 		}
-		else if (str[i] == 32 || str[i] == '\t' || str[i] == '\n' || str[i] == ',' || str[i] == ';' || str[i] == '.' || str[i] == '!' || str[i] == '?' || str[i] == '"' || str[i] == '(' || str[i] == ')' || str[i] == '{' || str[i] == '}')
+		for (j = 0 ; c[j] != '\0' ; j++)
 		{
-			if (str[i + 1] >= 97 && str[i + 1] <= 122)
+			if (str[i] == c[j])
 			{
-				str[i + 1] = str[i + 1] - 32;
+				if (str[i + 1] >= 97 && str[i + 1] <= 122)
+				{
+					str[i + 1] = str[i + 1] - 32;
+				}
 			}
 		}
 	}
