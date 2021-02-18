76d5078c78c315fa979f143d2d4670c42d55c9e3
SimonBr017 <simonbrard017@gmail.com>
2021-02-18 11:36:59 +0000

    add t3

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,18 @@
+#include "holberton.h"
+/**
+ *_strcmp - compare two string
+ *@s1: string
+ *@s2: string
+ *Return: 0 if same return else
+ */
+int _strcmp(char *s1, char *s2)
+{
+	int i;
+
+	for (i = 0 ; s1[i] != '\0' && s2[i] != '\0' ; i++)
+	{
+		if (s1[i] != s2[i])
+			return(s2[i] - s1[i]);
+	}
+	return (0);
+}
