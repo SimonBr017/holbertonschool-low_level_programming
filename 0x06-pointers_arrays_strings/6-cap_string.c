37cf5edd88b3f774145b8cf23ec11c8543d6c28b
SimonBr017 <simonbrard017@gmail.com>
2021-02-18 14:07:07 +0000

    add tmp t6

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,26 @@
+#include "holberton.h"
+/**
+ * cap_string - change by upper case every first char
+ *@str: string
+ *Return: str corected
+ */
+char *cap_string(char *str)
+{
+	int i;
+
+	for (i = 0; str[i] != '\0'; i++)
+	{
+		if (str[0] >= 97 && str[0] <= 122)
+		{
+			str[0] = str[0] - 32;
+		}
+		else if (str[i] == 32 || str[i] == '\t' || str[i] == '\n' || str[i] == ',' || str[i] == ';' || str[i] == '.' || str[i] == '!' || str[i] == '?' || str[i] == '"' || str[i] == '(' || str[i] == ')' || str[i] == '{' || str[i] == '}')
+		{
+			if (str[i + 1] >= 97 && str[i + 1] <= 122)
+			{
+				str[i + 1] = str[i + 1] - 32;
+			}
+		}
+	}
+	return (str);
+}
