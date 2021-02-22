d49528c758f1874fba88ff8c4a9bbba3aa19ade6
SimonBr017 <simonbrard017@gmail.com>
2021-02-22 13:25:34 +0000

    fix 3

---
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -0,0 +1,23 @@
+#include "holberton.h"
+/**
+ * *_strspn - get the length of a prefix substring
+ *@s : string
+ *@accept: string
+ *Return: i
+ */
+unsigned int _strspn(char *s, char *accept)
+{
+	int i, j;
+
+	for (i = 0 ; s[i] != '\0' ; i++)
+	{
+		for (j = 0 ; accept[j] != s[i] ; j++)
+		{
+			if  (accept[j] == 0)
+			{
+				return (i);
+			}
+		}
+	}
+	return (i);
+}
