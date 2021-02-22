e28ab3cd90c4324113220f7f604890d940599d95
SimonBr017 <simonbrard017@gmail.com>
2021-02-22 09:29:24 +0000

    add t2 holberton updat

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,21 @@
+#include "holberton.h"
+/**
+ * *_strchr - check in a char in a string
+ *@s: string
+ *@c: char
+ *Return: NULL if c is not found returns s if c is found
+ */
+char *_strchr(char *s, char c)
+{
+	int i = 0;
+
+	while (s[i])
+	{
+		if (s[i] == c)
+		{
+			return (s);
+		}
+		s++;
+	}
+	return (0);
+}
