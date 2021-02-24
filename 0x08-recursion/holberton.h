6b6bc839be2d695787f614a3a885c1851c1c4ebe
SimonBr017 <simonbrard017@gmail.com>
2021-02-24 15:00:30 +0000

    holberton update

--- 0x08-recursion/holberton.h
+++ 0x08-recursion/holberton.h
@@ -6,3 +6,7 @@ int factorial(int n);
 int _pow_recursion(int x, int y);
 int _sqrt_recursion(int n);
 int carre(int n, int x);
+int is_prime_number(int n);
+int checker(int n, int x);
+int is_palindrome(char *s);
+int check_pal(char *left, char *right);
