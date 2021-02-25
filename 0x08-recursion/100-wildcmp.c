d6b4032c731824d8273fc046879fe0d44d775413
SimonBr017 <simonbrard017@gmail.com>
2021-02-25 14:32:23 +0000

    add advance

---
+++ 0x08-recursion/100-wildcmp.c
@@ -0,0 +1,34 @@
+#include "holberton.h"
+/**
+ *wilcmp - compares two string
+ *@s1: string
+ *@s2: string
+ *Return: 1 or 0
+ */
+int wildcmp(char *s1, char *s2)
+{
+	if (*s1 == '\0' && *s2 == '\0')
+	{
+		return (1);
+	}
+	if (*s1 == *s2)
+	{
+		return (wildcmp((s1 + 1), (s2 + 1)));
+	}
+	if (*s2 == '*')
+	{
+		if (*(s2 + 1) == '*')
+		{
+			return (wildcmp(s1, (s2 + 1)));
+		}
+		else if (wildcmp(s1, (s2 + 1)) == 1)
+		{
+			return (1);
+		}
+		else
+		{
+			return (wildcmp((s1 + 1), s2));
+		}
+	}
+	return (0);
+}
--- 0x08-recursion/holberton.h
+++ 0x08-recursion/holberton.h
@@ -10,3 +10,4 @@ int is_prime_number(int n);
 int checker(int n, int x);
 int is_palindrome(char *s);
 int check_pal(char *left, char *right);
+int wildcmp(char *s1, char *s2);
