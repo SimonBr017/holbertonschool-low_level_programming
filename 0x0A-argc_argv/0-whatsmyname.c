06b4e112c8a79ba5acafd70a2658f0e9a6eff0b5
SimonBr017 <simonbrard017@gmail.com>
2021-02-26 11:36:29 +0000

    add T0 and README

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ * main - entry point
+ *@argc: arg count
+ *@argv: arg vecteur
+ *Return: returns 0 (sucsess)
+ */
+int main(int argc __attribute__((unused)), char **argv)
+{
+	printf("%s\n", argv[0]);
+	return (0);
+}
