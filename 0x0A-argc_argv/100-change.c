7e5bdb04e40a0a9c7af0c87116effe345aeadf21
SimonBr017 <simonbrard017@gmail.com>
2021-02-27 23:03:38 +0000

    fix betty T100

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -9,7 +9,7 @@
 int main(int argc, char **argv)
 {
 	int cents;
-        int i;
+	int i;
 
 	if (argc != 2)
 	{
@@ -27,25 +27,15 @@ int main(int argc, char **argv)
 	for (i = 0 ; cents > 0 ; i++)
 	{
 		if (cents >= 25)
-		{
 			cents = cents - 25;
-		}
 		else if (cents >= 10)
-		{
 			cents = cents - 10;
-		}
 		else if (cents >= 5)
-		{
 			cents = cents - 5;
-		}
 		else if (cents >= 2)
-		{
 			cents = cents - 2;
-		}
 		else if (cents == 1)
-		{
-			cents = cents - 1;
-		}
+		cents = cents - 1;
 	}
 	printf("%d\n", i);
 	return (0);
