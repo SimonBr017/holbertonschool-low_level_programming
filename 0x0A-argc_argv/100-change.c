ecb62d267ef8535ce07549ee5bbdbdd4ac3f2742
SimonBr017 <simonbrard017@gmail.com>
2021-02-27 06:11:09 +0000

    add tadvance

---
+++ 0x0A-argc_argv/100-change.c
@@ -0,0 +1,49 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - entry point
+ *@argc: arg count
+ *@argv: arg vecteur
+ *Return: returns 0 (sucsess) or 1 (error)
+ */
+int main(int argc, char **argv)
+{
+	int cents = atoi(argv[1]);
+	int i;
+
+	if (argc != 2)
+	{
+		printf("Error\n");
+		return (1);
+	}
+	if (argc < 0)
+	{
+		printf("0\n");
+		return (1);
+	}
+	for (i = 0 ; cents > 0 ; i++)
+	{
+		if (cents >= 25)
+		{
+			cents = cents - 25;
+		}
+		else if (cents >= 10)
+		{
+			cents = cents - 10;
+		}
+		else if (cents >= 5)
+		{
+			cents = cents - 5;
+		}
+		else if (cents >= 2)
+		{
+			cents = cents - 2;
+		}
+		else if (cents == 1)
+		{
+			cents = cents - 1;
+		}
+	}
+	printf("%d\n", i);
+	return (0);
+}
