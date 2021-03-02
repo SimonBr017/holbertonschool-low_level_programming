acecc216b74e844f9c337c951d27899fc1c5a2c6
SimonBr017 <simonbrard017@gmail.com>
2021-03-02 18:25:06 +0000

    few more

---
+++ 0x0B-malloc_free/100-strtow.c
@@ -0,0 +1,19 @@
+#include "holberton.h"
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * **strtow - split spring into words
+ *@str: string
+ *Return: null if str = null or str = "", 
+ */
+char **strtow(char *str)
+{
+        char **dest;
+
+        dest = 0;
+        if (str == '\0' || str == NULL)
+        {
+                return (NULL);
+        }
+        return (dest);
+}
