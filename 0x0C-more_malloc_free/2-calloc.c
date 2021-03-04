ed9897fee647d09b47f1e6a2fe27eb02f0716fa6
SimonBr017 <simonbrard017@gmail.com>
2021-03-04 11:01:44 +0000

    add t2 & holberton.h

---
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -0,0 +1,25 @@
+#include "holberton.h"
+#include <stdlib.h>
+/**
+ * *_calloc - allocates memory for an array of nmemb elements of size bytes
+ *@nmemb: unsigned int
+ *@size: unsigned int
+ *Return:  returns a pointer to the allocated memory
+ */
+void *_calloc(unsigned int nmemb, unsigned int size)
+{
+	int *p;
+	unsigned int i;
+
+	if (nmemb == 0 || size == 0)
+		return (NULL);
+
+	p = malloc(nmemb * size);
+	if (p == NULL)
+		return (NULL);
+
+	for (i = 0; i < nmemb; i++)
+		p[i] = 0;
+
+	return (p);
+}
