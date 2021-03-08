7cfe48e43c6decf34cfdd2dd8e168039c3c3688f
SimonBr017 <simonbrard017@gmail.com>
2021-03-08 13:41:00 +0000

    forgote typedef

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -8,7 +8,7 @@
  *
  *Description: struct def dog
  */
-struct dog
+typedef struct dog
 {
 	char *name;
 	float age;
@@ -16,4 +16,6 @@ struct dog
 } dog_t;
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
+dog_t *new_dog(char *name, float age, char *owner);
+void free_dog(dog_t *d);
 #endif
