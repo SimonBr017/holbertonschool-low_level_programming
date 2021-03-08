8413f9114950036763dabacefe1c71f4c534ccd2
SimonBr017 <simonbrard017@gmail.com>
2021-03-08 12:55:48 +0000

    add folder t0 and readme

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,17 @@
+#ifndef DOG_H
+#define DOG_H
+/**
+ * struct dog - struct def
+ *@name: string of char
+ *@age: float
+ *@owner: string of char
+ *
+ *Description: struct def dog
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+#endif
