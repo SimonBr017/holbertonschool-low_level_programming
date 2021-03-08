080e892697cfa88fc6f013b31a085d3289419392
SimonBr017 <simonbrard017@gmail.com>
2021-03-08 14:46:18 +0000

    fix betty T4

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -36,7 +36,7 @@ char *_strcpy(char *dest, char *src)
  *@name: string of char
  *@age: float
  *@owner: string of char
- *Return:
+ *Return: pointer to n_dog
  */
 dog_t *new_dog(char *name, float age, char *owner)
 {
