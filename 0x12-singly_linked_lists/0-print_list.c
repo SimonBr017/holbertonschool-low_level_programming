5085259c089c80f6583b700838363bd91945eaaf
SimonBr017 <simonbrard017@gmail.com>
2021-03-18 15:04:08 +0000

    move i++ inside the boucle in t0

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -7,7 +7,7 @@
  */
 size_t print_list(const list_t *h)
 {
-	int i = 1;
+	int i = 0;
 
 	if (h == NULL)
 		return (0);
@@ -20,7 +20,7 @@ size_t print_list(const list_t *h)
 			printf("[%d] %s\n", h->len, h->str);
 		}
 		h = h->next;
+		i++;
 	}
-	i++;
 	return (i);
 }
