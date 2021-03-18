c04d78abdd3d60ef40c3221906bb1fde6f1fc19f
SimonBr017 <simonbrard017@gmail.com>
2021-03-18 15:20:35 +0000

    fix betty t1

--- 0x12-singly_linked_lists/1-list_len.c
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -7,15 +7,14 @@
  */
 size_t list_len(const list_t *h)
 {
-        int i = 0;
+	int i = 0;
 
-        if (h == NULL)
-                return (0);
-        while (h != NULL)
-        {
-
-                h = h->next;
-                i++;
-        }
-        return (i);
+	if (h == NULL)
+		return (0);
+	while (h != NULL)
+	{
+		h = h->next;
+		i++;
+	}
+	return (i);
 }
