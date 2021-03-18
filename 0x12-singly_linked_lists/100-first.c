000a94560774b48bb5433ab29eb746e98fe372ee
SimonBr017 <simonbrard017@gmail.com>
2021-03-18 17:04:01 +0000

    fix t100 advance

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -1,12 +1,12 @@
 #include <stdio.h>
 
 
-void print_something(void) __atribute__((constructor));
+void print_something(void) __attribute__((constructor));
 /**
  *print_something - print something before the main
  */
 void print_something(void)
 {
-	printf("You're beat! and yet, you must allow,\n"
-	       "I bore my house upon my back!\n");
+	printf("You're beat! and yet, you must allow,\n");
+	printf("I bore my house upon my back!\n");
 }
