2c5fcd337b46d55cfa64ea8aa774341832a5a550
SimonBr017 <simonbrard017@gmail.com>
2021-03-18 15:17:27 +0000

    add t1

---
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -0,0 +1,21 @@
+#include "lists.h"
+
+/**
+ * print_list - function that prints all the elements of a list_t
+ *@h: pointer to the struct
+ *Return: the number of nodes.
+ */
+size_t list_len(const list_t *h)
+{
+        int i = 0;
+
+        if (h == NULL)
+                return (0);
+        while (h != NULL)
+        {
+
+                h = h->next;
+                i++;
+        }
+        return (i);
+}
