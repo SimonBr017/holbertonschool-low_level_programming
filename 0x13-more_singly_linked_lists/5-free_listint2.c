339e5dc2f8240f3568dd11a8c1a2db9707e57954
SimonBr017 <simonbrard017@gmail.com>
2021-03-23 10:38:42 +0000

    fix t5

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -7,9 +7,7 @@ void free_listint2(listint_t **head)
 {
 	listint_t *tmp;
 
-	if (head == NULL)
-		return;
-	while (head != NULL)
+	while (head != NULL && *head != NULL)
 	{
 		tmp = *head;
 		*head = (*head)->next;
