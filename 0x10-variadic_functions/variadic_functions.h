7cafa43762ee0297f7e89f375feda1e5e381c223
SimonBr017 <simonbrard017@gmail.com>
2021-03-29 11:24:17 +0000

    fix variadic.h

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -3,6 +3,17 @@
 #include <stdarg.h>
 #include <stdio.h>
 #include <stddef.h>
+/**
+ * struct format - structure of our function pointer and paramters
+ * @form: format parameter
+ * @f: function pointer
+ */
+
+typedef struct format
+{
+	char *form;
+	void (*f)();
+} form_t;
 int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
 void print_strings(const char *separator, const unsigned int n, ...);
