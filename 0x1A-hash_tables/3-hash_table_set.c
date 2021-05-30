943f733237a8a25114ed7c65c61b8e52c9be23cf
SimonBr017 <simonbrard017@gmail.com>
2021-05-30 14:32:04 +0200

    add t3 fix maybe

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -11,14 +11,12 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 	int index = 0;
 	hash_node_t *node, *new_elem;
 
-
 	if (ht == NULL || key == NULL || value == NULL)
 		return (0);
-
 	index = key_index((const unsigned char *) key, ht->size);
 	node = ht->array[index];
 
-	/*while (node != NULL)
+	while (node != NULL)
 	{
 		if (strcmp(key, node->key) == 0)
 		{
@@ -27,22 +25,14 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 			return (1);
 		}
 		node = node->next;
-	}*/
-
+	}
 	new_elem = malloc(sizeof(hash_node_t));
 	if (new_elem == NULL)
 		return (0);
-
 	new_elem->key = strdup(key);
 	new_elem->value = strdup(value);
 	new_elem->next = NULL;
-	
-	if (new_elem->value == NULL)
-	{
-		free(new_elem->key);
-		free(new_elem);
-		return (0);
-	}
+
 	if (node != NULL)
 	{
 		while (node->next != NULL)
