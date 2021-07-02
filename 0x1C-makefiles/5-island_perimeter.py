9b99cbff9468234387f6d859a19f6bd7970407ba
SimonBr017 <simonbrard017@gmail.com>
2021-07-02 14:58:24 +0200

    fix pep8

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -6,20 +6,21 @@ Island preimeter calculator
 
 def island_perimeter(grid):
     """defition of island-perimeter"""
+
     peri = 0
 
     for row in range(len(grid)):
-        for column in range(len(grid[row])):
-            if grid[row][column] == 1:
-                if row == 0 or grid[row - 1][column] == 0:
+        for col in range(len(grid[row])):
+            if grid[row][col] == 1:
+                if row == 0 or grid[row - 1][col] == 0:
                     peri += 1
 
-                if column == 0 or grid[row][column - 1] == 0:
+                if col == 0 or grid[row][col - 1] == 0:
                     peri += 1
 
-                if row == (len(grid) - 1) or grid[row + 1][column] == 0:
+                if row == (len(grid) - 1) or grid[row + 1][col] == 0:
                     peri += 1
 
-                if column == (len(grid[row]) - 1) or grid[row][column + 1] == 0:
+                if col == (len(grid[row]) - 1) or grid[row][col + 1] == 0:
                     peri += 1
     return peri
