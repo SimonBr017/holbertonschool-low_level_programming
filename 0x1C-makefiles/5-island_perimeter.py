ca19a00417fc41884b70bddbd16aec520dc4e4bd
SimonBr017 <simonbrard017@gmail.com>
2021-07-02 14:34:38 +0200

    add t5

---
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+"""
+Island preimeter calculator
+"""
+
+
+from typing import Collection
+
+
+def island_perimeter(grid):
+
+    peri = 0
+
+    for row in range(len(grid)):
+        for column in range(len(grid[row])):
+            if grid[row][column] == 1:
+                if row == 0 or grid[row - 1][column] == 0:
+                    peri += 1
+
+                if column == 0 or grid[row][column - 1] == 0:
+                    peri += 1
+
+                if row == len(grid) - 1 or grid[row + 1][column] == 0:
+                    peri += 1
+
+                if column == len(grid[row]) - 1 or grid[row][column + 1]:
+                    peri += 1
+    return peri + 1
